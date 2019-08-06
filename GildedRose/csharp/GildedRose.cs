b83556ae151c68ed2fb5b9f799b7ba7ed63a73b2
MauroGiuffo <maurogiuffo@gmail.com>
2019-08-06 17:40:46 -0300

    * GildedRose testcases

--- GildedRose/csharp/GildedRose.cs
+++ GildedRose/csharp/GildedRose.cs
@@ -14,13 +14,33 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
+                
+                if (Items[i].Quality > 50 && Items[i].Name !="Sulfuras, Hand of Ragnaros")
+                    Items[i].Quality = 50;
+                
+                if (Items[i].Quality < 0)
+                    Items[i].Quality = 0;
+                
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
+                    
+                    
+                    
+                    
+                    
                     if (Items[i].Quality > 0)
                     {
                         if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            if (Items[i].Name != "Conjured Mana Cake")
+                            {
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
+                            else
+                            {
+                                if(Items[i].Quality > 1)
+                                    Items[i].Quality = Items[i].Quality - 2;
+                            }
                         }
                     }
                 }
--- GildedRose/csharp/GildedRoseTest.cs
+++ GildedRose/csharp/GildedRoseTest.cs
@@ -14,5 +14,157 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual("foo", Items[0].Name);
         }
+
+        [Test]
+        public void LowerItemValues()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(9, item.SellIn);
+                Assert.AreEqual(9, item.Quality);
+            }
+            
+        }
+
+
+        [Test]
+        public void DegradeTwiceAsFast()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(8, item.Quality);
+            }
+        }
+        
+        [Test]
+        public void QualityNeverNegative()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(0, item.Quality);
+            }
+        }
+        
+        
+        [Test]
+        public void AgedBrieIncreasesQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(11, item.Quality);
+            }
+        }
+        
+        
+        [Test]
+        public void QualityNeverMore50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(50, item.Quality);
+            }
+        }
+        
+        [Test]
+        public void SulfurasNeverHasToBeSold()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(10, item.SellIn);
+            }
+        }
+     
+        [Test]
+        public void SulfurasNotDecreasesQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(10, item.Quality);
+            }
+        }
+        
+        [Test]
+        public void BackstagePassesIncreases2QualityLess10Days()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(12, item.Quality);
+            }
+        }
+        
+        
+        [Test]
+        public void BackstagePassesIncreases3QualityLess5Days()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(13, item.Quality);
+            }
+        }
+
+         
+        [Test]
+        public void BackstagePassesDropsTo0QualityAfterSellIn()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(0, item.Quality);
+            }
+        }
+        
+        [Test]
+        public void ConjuredDegradeQualityTwiceAsFast()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            foreach (var item in Items)
+            {
+                Assert.AreEqual(8, item.Quality);
+            }
+        }
+        
+     
     }
 }
